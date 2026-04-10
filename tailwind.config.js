e07d3e81d0682b4e1cd06befc90b55bae1864aa9
Chambatina1 <geochambatina@gmail.com>
2026-04-10 12:27:31 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,10 @@
-
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Esto le dice a Tailwind dónde buscar clases CSS usadas en tus componentes
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
